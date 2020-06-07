b8f0a7a541bb17b5f1d21e255e14706ca7436787
kenta.ozawa <ozkn666@yahoo.co.jp>
2020-06-07 13:42:33 +0900

    カレンダーに曜日を追加

--- front/src/components/CalendarBoard/index.jsx
+++ front/src/components/CalendarBoard/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { GridList } from '@material-ui/core'
+import { GridList, Typography } from '@material-ui/core'
 import * as styles from './style.css'
 
 import CalendarElement from '../CalendarElement'
@@ -7,10 +7,25 @@ import { createCalendar } from '../../service/calendar'
 
 const calendar = createCalendar()
 
+const days = ["日", "月", "火", "水", "木", "金", "土"]
+
 const CalendarBoard = () => {
   return (
-    <div className="container">
+    <div className={styles.container}>
       <GridList className={styles.grid} cols={7} spacing={0} cellHeight="auto">
+        {days.map(d => (
+          <li key={d}>
+            <Typography
+              className={styles.days}
+              color="textSecondary"
+              align="center"
+              variant="caption"
+              component="div"
+            >
+              {d}
+            </Typography>
+          </li>
+        ))}
         {calendar.map(c => (
           <li key={c.toISOString()}>
             <CalendarElement day={c} />
